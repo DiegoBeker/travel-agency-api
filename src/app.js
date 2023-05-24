138bf74586552c2f0894e99ca10f59d06aa1d69b
Diego <diegobecker2007@hotmail.com>
2023-05-24 09:21:04 -0300

    chore: create project structure

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/index.routes.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, ()=> console.log(`Server running on ${PORT}`));
